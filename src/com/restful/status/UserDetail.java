dfdbe3981f5892febddc0d6d6e6cbfef217f1bc0
divyachandra <justdiv@gmail.com>
2017-02-06 16:11:52 -0800

    @PathParam
    Added method to demonstra

--- src/com/restful/status/UserDetail.java
+++ src/com/restful/status/UserDetail.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -25,13 +26,47 @@ import com.restful.utils.ToJson;
  * @author Divya
  * @date 06-Feb-2017
  */
-@Path("/user")
+@Path("/userdetails")
 public class UserDetail {
 	public static final String FIND_BY_NAME_SQL = "Select USERID, USERNAME, PASSWORD, FIRSTNAME, LASTNAME from USER where USERNAME=?";
 
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String returnUserDetail(@QueryParam("user") String user) throws SQLException, JSONException {
+	public Response returnUserDetail(@QueryParam("user") String user) throws SQLException, JSONException {
+
+		PreparedStatement prepStatement = null;
+		String result = null;
+		Response response = null;
+
+		try {
+
+			if (user == null)
+				return Response.status(404).entity("Error: No user entered").build();
+
+			Connection connection = DatabaseUtil.getConnection();
+			prepStatement = connection.prepareStatement(FIND_BY_NAME_SQL);
+			prepStatement.setString(1, user);
+			ResultSet resultSet = prepStatement.executeQuery();
+
+			ToJson jsonConverter = new ToJson();
+			JSONArray jsonArray = new JSONArray();
+
+			jsonArray = jsonConverter.convertToJsonArray(resultSet);
+			result = jsonArray.toString();
+			response = Response.ok(result).build();
+			prepStatement.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return response;
+
+	}
+
+	@Path("/{user}")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Response getUserByPath(@PathParam("user") String user) throws SQLException, JSONException {
 
 		PreparedStatement prepStatement = null;
 		String result = null;
@@ -54,7 +89,7 @@ public class UserDetail {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return result;
+		return response;
 
 	}
 
